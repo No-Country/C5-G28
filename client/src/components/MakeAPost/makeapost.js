4568a1c4d3aa57bd14b0ee2f990bcdcba59a9b70
cristian <cristiangomez.dev@gmail.com>
2022-06-11 09:23:12 -0300

    postear by cristian gomez working on dev branch

--- client/src/components/MakeAPost/makeapost.js
+++ client/src/components/MakeAPost/makeapost.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React,{useContext} from 'react';
 import "../../styles/makeapost.css"
-
-
-
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { StoreContext } from "../../store/storeProvider";
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 const MakeAPost = () => {
+    const MySwal = withReactContent(Swal)
+    const { user } = useContext(StoreContext);
+
+    let Navigate = useNavigate();
+
+     const {handleSubmit,errors,touched,getFieldProps} = useFormik({
+    initialValues:{
+        username:'',
+        title:'',
+        category:'',
+        content:'',
+        urlPhoto:''
+    },
+    onSubmit:values => {
+        try{
+            console.log(values)
+            axios.post(
+                'http://localhost:3001/api/post/create',
+                {username:user.user.username,title:values.title,category:values.category,content:values.content,urlPhoto:values.content}
+                ).then(res => {
+                    
+                    MySwal.fire({title:<h2> Posteado </h2>}).then(()=>{Navigate('/home')})
+                }).catch(error => MySwal.fire({title:<h2> fallo post </h2>}))
+        }catch(error){
+            console.log(error)
+        } 
+    },
+    validationSchema:Yup.object({
+        username:Yup.string().required('username requerido'),
+        title:Yup.string().required('title requerido'),
+        category:Yup.string().required('categoria requerida'),
+        content:Yup.string().required('content requerido'),
+        urlPhoto:Yup.string().required('urlPhoto requerido')
+    })
+})
     return (
     
     <section className='my-fkng-container'>
@@ -21,11 +60,11 @@ const MakeAPost = () => {
 
                 {/* TÍTULO */}
                 <label className="d-flex">
-                    <input type="text" name="title" placeholder='Escribe un título aquí...' className='my-input-form my-title-input '/>
+                    <input type="text" name="title" {...getFieldProps('title')} placeholder='Escribe un título aquí...' className='my-input-form my-title-input '/>
                 </label>
 
                 {/* ETIQUETAS */}
-                <select name="item" size={1} className="d-flex form-control-lg my-title-input">
+                <select name="item" size={1} {...getFieldProps('category')} className="d-flex form-control-lg my-title-input">
                     <option value="" disabled selected>Selecciona una categoría para tu post</option>
                     <option value="js">JavaScript</option>
                     <option value="react">React</option>
@@ -36,12 +75,12 @@ const MakeAPost = () => {
 
 
                 {/* TEXTAREA */}
-                <textarea name="textarea" rows="12" className='my-input-form my-text-area' placeholder='Escribe el contenido del artículo aquí...'></textarea>
+                <textarea name="textarea" {...getFieldProps('content')} rows="12" className='my-input-form my-text-area' placeholder='Escribe el contenido del artículo aquí...'></textarea>
 
 
                 {/* CARGAR ENLACES */}
                 <label className="d-flex">
-                    <input type="text" name="enlace" placeholder='Ingresa una url de imágen aquí...' className='my-input-form my-input-url'/>
+                    <input type="text" {...getFieldProps('urlPhoto')} placeholder='Ingresa una url de imágen aquí...' className='my-input-form my-input-url'/>
                 </label>
                 
 
--- client/src/components/Sidebar/sidebar.js
+++ client/src/components/Sidebar/sidebar.js
@@ -1,4 +1,4 @@
-import React,{useContext,useEffect} from "react";
+import React,{useContext} from "react";
 import "./sidebar.css";
 
 //importando Link desde react route para poder navegar entre las secciones
